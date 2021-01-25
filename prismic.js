923106bd94825eac8356be5f09ef3e28d32008e3
Davey Moores <daveymoores@gmail.com>
2021-01-25 07:45:07 +0100

    fixing formatting on uid pages file and prismic.js

--- prismic.js
+++ prismic.js
@@ -1,35 +1,34 @@
-/** Example file */
+import Link from "next/link";
+import Prismic from "prismic-javascript";
+import React from "react";
 
-import Prismic from 'prismic-javascript'
-import Link from 'next/link'
+import smConfig from "./sm.json";
 
-import smConfig from './sm.json'
-
-export const apiEndpoint = smConfig.apiEndpoint
+export const apiEndpoint = smConfig.apiEndpoint;
 
 // -- Access Token if the repository is not public
 // Generate a token in your dashboard and configure it here if your repository is private
-export const accessToken = ''
+export const accessToken = "";
 
 // -- Link resolution rules
 // Manages the url links to internal Prismic documents
 export const linkResolver = (doc) => {
-  if (doc.type === 'post') {
-    return `/blog/${doc.uid}`
+  if (doc.type === "post") {
+    return `/blog/${doc.uid}`;
   }
-  if (doc.type === 'page') {
-    return `/${doc.uid}`
+  if (doc.type === "page") {
+    return `/${doc.uid}`;
   }
-  return '/'
-}
+  return "/";
+};
 
 // Additional helper function for Next/Link components
 export const hrefResolver = (doc) => {
-  if (doc.type === 'post') {
-    return '/blog/[uid]'
+  if (doc.type === "post") {
+    return "/blog/[uid]";
   }
-  return '/'
-}
+  return "/";
+};
 
 export const customLink = (type, element, content, children, index) => (
   <Link
@@ -39,17 +38,19 @@ export const customLink = (type, element, content, children, index) => (
   >
     <a>{content}</a>
   </Link>
-)
+);
 
 export const Router = {
-  routes: [{"type":"page","path":"/:uid"}],
+  routes: [{ type: "page", path: "/:uid" }],
 
   href: (type) => {
-    const route = Router.routes.find(r => r.type === type);
+    const route = Router.routes.find((r) => r.type === type);
     return route && route.href;
-  }
+  },
 };
 
-export const Client = (req = null, options = {}) => (
-  Prismic.client(apiEndpoint, Object.assign({ routes: Router.routes }, options))
-)
+export const Client = (req = null, options = {}) =>
+  Prismic.client(
+    apiEndpoint,
+    Object.assign({ routes: Router.routes }, options)
+  );
