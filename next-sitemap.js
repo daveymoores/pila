a443b0f9cbb8f0b9f3a2d891d60dcc5dcf5af7e0
Davey Moores <daveymoores@gmail.com>
2021-05-28 16:07:14 +0200

    adding pre-commit hook for sitemap

--- next-sitemap.js
+++ next-sitemap.js
@@ -1,5 +1,8 @@
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl:
+    process.env.NODE_ENV !== "development"
+      ? process.env.SITE_URL
+      : process.env.NEXT_PUBLIC_VERCEL_URL,
   generateRobotsTxt: true,
   exclude: ["/account", "/account/sessions"],
 };
