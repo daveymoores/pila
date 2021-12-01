09abef7fa2f365812e602f7330e72471c0d5620f
Davey Moores <daveymoores@gmail.com>
2021-12-01 09:30:51 +0100

    removing layout functions

--- src/organisms/account-layout/AccountLayout.tsx
+++ src/organisms/account-layout/AccountLayout.tsx
@@ -5,39 +5,6 @@ import { Theme } from "../../../types/Theme";
 import Section from "../../layout/section/Section";
 import HeroText from "../hero-text/HeroText";
 
-const columns = {
-  small: Array(4).fill("flex"),
-  medium: Array(8).fill("flex"),
-  large: Array(12).fill("flex"),
-  xlarge: Array(12).fill("flex"),
-};
-
-const rows = {
-  small: ["auto", "auto"],
-  medium: ["auto", "auto"],
-  large: ["auto"],
-  xlarge: ["auto"],
-};
-
-const areas = {
-  small: [
-    { name: "menu", start: [0, 0], end: [3, 0] },
-    { name: "content", start: [0, 1], end: [3, 1] },
-  ],
-  medium: [
-    { name: "menu", start: [0, 0], end: [3, 0] },
-    { name: "content", start: [0, 1], end: [7, 1] },
-  ],
-  large: [
-    { name: "menu", start: [0, 0], end: [2, 0] },
-    { name: "content", start: [3, 0], end: [11, 0] },
-  ],
-  xlarge: [
-    { name: "menu", start: [0, 0], end: [2, 0] },
-    { name: "content", start: [3, 0], end: [11, 0] },
-  ],
-};
-
 interface AccountLayoutProps {
   greeting: string;
   title: string;
